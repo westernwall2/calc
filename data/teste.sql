1152b156b8de6e77f991aa9437c9dca2f840e550
westernwall2 <jefersoncosta1994@gmail.com>
2022-06-27 21:44:44 -0300

    teste 2 sql

--- data/teste.sql
+++ data/teste.sql
@@ -1,1 +1,11 @@
 select * from tabela_teste
+insert into tabela_teste values 
+update tabela_teste set ex = 1 where ex > 0
+
+select * from tabela_teste
+insert into tabela_teste values 
+update tabela_teste set ex = 1 where ex > 0
+
+select * from tabela_teste
+insert into tabela_teste values 
+update tabela_teste set ex = 1 where ex > 0
